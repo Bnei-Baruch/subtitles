6ebcbd6ab32e95139cb0814edb6992de7cd1406f
Gregory Shilin <gshilin@gmail.com>
2021-02-23 03:34:05 +0200

    Fix deletion + misc renames

--- src/reducers/subtitles.js
+++ src/reducers/subtitles.js
@@ -1,5 +1,4 @@
 import * as actionType from '../constants/actionTypes';
-import { SET_CURRENT_BOOK } from '../constants/actionTypes';
 
 const initialState = {
   broadcast: false,
@@ -14,17 +13,17 @@ const subtitlesReducer = (state = initialState, action) => {
   switch (type) {
   case actionType.SET_BROADCAST:
     return { ...state, broadcast: !state.broadcast };
-  case actionType.ADD_FILE:
-    const { file } = action;
-    return { ...state, books: [...state.books, file] };
-  case actionType.DELETE_FILES:
-    const { names } = action;
-    const rows      = state.books.filter(row => !names.includes(row.name));
-    return { ...state, rows };
-  case actionType.SET_SELECTED:
+  case actionType.ADD_BOOK:
+    const { book } = action;
+    return { ...state, books: [...state.books, book] };
+  case actionType.DELETE_BOOKS:
+    const { ids } = action;
+    const books   = state.books.filter(book => !ids.includes(book.id));
+    return { ...state, books };
+  case actionType.SET_SELECTED_BOOK:
     return { ...state, selected: action.selected };
   case actionType.SET_CURRENT_BOOK:
-    const { bookId } = action;
+    const { bookId }  = action;
     const currentBook = state.books.filter((book) => book.id === bookId);
     return { ...state, currentBook: currentBook?.[0] };
   default:
